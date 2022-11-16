e00818c36a5e85e512a8840142b00f0efba14bca
psh <pshysd@kakao.com>
2022-11-16 16:19:40 +0900

    dtd setting

---
+++ Spring_Project/src/main/java/com/kh/spring/member/model/vo/Member.java
@@ -0,0 +1,11 @@
+package com.kh.spring.member.model.vo;
+
+public class Member {
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String gender;
+	private String email;
+	private String address;
+}
